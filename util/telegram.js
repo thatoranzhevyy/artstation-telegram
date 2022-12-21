89baf8601411b12f44b1eaaddd91973e083e0eca
Bogenbai Bayzharassov <thatoranzhevyy@gmail.com>
2022-12-21 19:33:20 +0600

    перенос токена и id в файл .env

--- util/telegram.js
+++ util/telegram.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
+
+console.log()
 const {Telegraf} = require('telegraf');
-const bot = new Telegraf('1489066598:AAE7n6ILaQ_-gMZ9usA4QIKzLwmiTdaIFXQ');
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 const tgSender = {
   /**
    * Личное сообщение
    * */
   sendPrivateMassage(msg) {
-    bot.telegram.sendMessage(888799838, msg);
+    bot.telegram.sendMessage(process.env.TELEGRAM_USER_ID, msg);
   },
   sendPost(data) {
-    return bot.telegram.sendMediaGroup(-1001868760290, data)
+    return bot.telegram.sendMediaGroup(process.env.TELEGRAM_GROUP_ID, data)
   }
 }
 
